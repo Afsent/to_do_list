b667425c6b23d6692753b9b613ac390d39ef6550
Afsent <aleexzhuravlev@gmail.com>
2020-01-22 14:09:57 +0300

    Inital commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import sqlite3
+from bottle import route, run, template
+
+@route('/todo')
+def todo_list():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
+    result = c.fetchall()
+    c.close()
+    output = template('table', rows=result)
+    return output
+
+run(reloader=True)
