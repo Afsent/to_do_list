a1dac6dbc5f2c9bf4d1dfe0dd17e9434a797750c
Afsent <aleexzhuravlev@gmail.com>
2020-01-22 14:32:44 +0300

    Add task creation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from bottle import route, run, template
+from bottle import route, run, template, request
+
 
 @route('/todo')
 def todo_list():
@@ -10,5 +11,27 @@ def todo_list():
     c.close()
     output = template('table', rows=result)
     return output
+
+
+@route('/new', method='GET')
+def new_item():
+    if request.GET.save:
+
+        new = request.GET.task.strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new, 1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()
+
+        return f'<p>Новая задача создана, номер задачи: {new_id}</p>'
+    else:
+        return template('new_task.tpl')
+
+
+
 
 run(reloader=True)
